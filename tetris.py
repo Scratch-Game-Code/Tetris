44650fc3bdd7431b50703ab31f656c35c9686734
Timmy Konick <konick781@gmail.com>
2013-10-04 19:30:51 -0400

    fixed drop bug

--- tetris.py
+++ tetris.py
@@ -240,26 +240,38 @@ class GameScreen(object):
             else:
                 new_brd.append(node)
         pygame.display.flip()
-        board = new_brd[:]
+        board = new_brd
         time.sleep(0.1)
-        displaced = list()
+        displaced = dict()
         for node in board:
             surface = node.keys()[0]
-            if node[surface][1] < max(line_amnt):
-                displaced.append(node[surface])
-        while all(blk.bottom < 740 for blk in displaced):
+            if node[surface][1] < max(line_amnt): 
+                displaced[surface] = node[surface]
+        while displaced:
             self.screen.blit(self.brd, (40, 20))
+            displaced  = self.displace_blocks(displaced, board)
             for blk in displaced:            
-                blk.move_ip([0, 2.0]) 
+                displaced[blk].move_ip([0, 2.0])
             self.update_board(board) 
-            for node in board:
-                surface = node.keys()[0]
-                if any(blk.left == node[surface].left and
-                       blk.bottom == node[surface].top and
-                       node[surface] not in displaced for blk in displaced): 
-                    return board
             pygame.display.flip()
         return board
+
+    def displace_blocks(self, dpl, board):
+        stop_blk = dict()
+        for node in board:
+            surface = node.keys()[0]
+            for blk in dpl:
+                if (dpl[blk].bottom >= 740 or 
+                    dpl[blk].left == node[surface].left and 
+                    dpl[blk].bottom == node[surface].top and
+                    surface not in dpl or
+                    dpl[blk].left == node[surface].left and
+                    dpl[blk].bottom == node[surface].top and
+                    surface in stop_blk):
+                    stop_blk[blk] = dpl[blk]
+        for blk in stop_blk:
+            del dpl[blk]
+        return dpl
 
     @property   
     def over(self):
@@ -346,7 +358,8 @@ class Tetris(object):
     def vertical_clearance(self, cur_brd):
         for node in cur_brd:
             for blk in node:
-                if any(self.blocks[v].bottom + self.speed[1] > node[blk].top for v in self.blocks):
+                if any(self.blocks[v].bottom + self.speed[1] > node[blk].top 
+                       for v in self.blocks):
                     self.speed[1] = self.levels[self.level][1]
                     return
         return
@@ -481,7 +494,8 @@ def main():
                     sys.exit()
             if tetris.speed[1]:
                 tetris.lateral_clearance(cur_brd)
-                tetris.vertical_clearance(cur_brd)
+                if tetris.speed[1] == 10:
+                    tetris.vertical_clearance(cur_brd)
                 for i in tetris.blocks:
                     tetris.blocks[i].move_ip(tetris.speed)
             else:
